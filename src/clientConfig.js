c1a7afdbaddc7f385008efef13581de580089bc3
zhangyuezhong <yuezhong.zhang@team.telstra.com>
2024-08-20 23:07:02 +1000

    update css

--- src/clientConfig.js
+++ src/clientConfig.js
@@ -1,10 +1,13 @@
+const location = new URL(window.location.href);
+const baseUrl = location.origin;
+
 export const clientConfig = {
+  flowId: "53712bc0-21de-42d6-b416-a64d1ed3c2c5",
   clientId: "42952baf-a65f-4147-ad8b-56441794abc4",
-  redirectUri: "https://purple-network.github.io/mywidget/",
+  redirectUri: baseUrl + import.meta.env.BASE_URL,
   // opts: {
   //   org: "",
   //   provider: "",
   //   target: "",
   // },
-  flowId: "53712bc0-21de-42d6-b416-a64d1ed3c2c5",
 };
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "widget",
+    chunkSizeWarningLimit: 3000,
   },
   base: "/mywidget/",
 });
