b11396fe6f104f66a59e892194a79814992f3968
zhangyuezhong <yuezhong.zhang@team.telstra.com>
2024-08-21 12:15:38 +1000

    Update genesysCloudUtils.js

--- src/utils/genesysCloudUtils.js
+++ src/utils/genesysCloudUtils.js
@@ -1,19 +1,18 @@
 import platformClient from "purecloud-platform-client-v2";
 
 const client = platformClient.ApiClient.instance;
-client.setPersistSettings(true);
 client.setEnvironment(platformClient.PureCloudRegionHosts.ap_southeast_2);
+client.setPersistSettings(true);
 const usersApi = new platformClient.UsersApi();
 const conversationApi = new platformClient.ConversationsApi();
 
 export async function authenticate(clientConfig) {
   try {
-    const data = await client.loginImplicitGrant(
+    return await client.loginImplicitGrant(
       clientConfig.clientId,
       clientConfig.redirectUri,
       clientConfig.opts
     );
-    return data;
   } catch (err) {
     console.error(err);
     throw err;
